1da7b4a3960e863a5117e697ba2794f7d51183d2
Hesham <mohamed.heshmse@gmail.com>
2025-09-24 17:56:32 +0200

    update readme and scripts

--- scripts/train_t2l_mistral.sh
+++ scripts/train_t2l_mistral.sh
@@ -5,6 +5,6 @@ WANDB_MODE=disabled uv run python scripts/train_custom_sft.py \
     --warmup_frac=0.2 --lr=2.5e-5 --n_ds_per_batch=8 \
     --n_points_per_task=1 --grad_accum_steps=1 \
     --epochs=2000 --n_descs_per_ds=128 --n_train_ds=479 \
-    --exp_setup=z_hyper_lora --encoder_type=linear \
+    --exp_setup=hyper_lora --encoder_type=linear \
     --l2_reg_generated_w=1e-3 --label_smoothing=0.1 \
     --neftune_noise_alpha=5 --weight_decay=1e-2
